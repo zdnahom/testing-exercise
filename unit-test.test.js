78f66e23c8190324957a2121239c56664efc5400
Nahom <zerihunnahom@gmail.com>
2023-03-01 13:01:30 +0300

    Add new testcase

--- unit-test.test.js
+++ unit-test.test.js
@@ -1,5 +1,9 @@
 const exerciseOne=require('./exercise-1')
 
-it("length equals 5",()=>{
+it("length meet the conditions",()=>{
     expect(exerciseOne("nahom")).toBe(5);
+})
+it("condition not met",()=>{
+    expect(()=>exerciseOne("")).toThrow(Error)
+    expect(()=>exerciseOne("nahomzerihun")).toThrow(Error)
 })
