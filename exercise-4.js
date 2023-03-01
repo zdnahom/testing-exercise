f976996d31e3d068c34c632250225e34d673ec08
Nahom <zerihunnahom@gmail.com>
2023-03-01 13:59:36 +0300

    Update capitalize function

--- exercise-4.js
+++ exercise-4.js
@@ -1,4 +1,8 @@
 const capitalize=(str)=>{
-    return str[0].toUpperCase() + str.slice(1)
+    if(typeof str==="string"){
+        return str.length!==0?str[0].toUpperCase() + str.slice(1):"Empty string"
+      }else{
+        throw new Error("Not a string")
+      }
 }
 module.exports=capitalize
