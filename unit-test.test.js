c0c9557581e69acc52754787fa1de22be36ed79a
Nahom <zerihunnahom@gmail.com>
2023-03-01 13:08:57 +0300

    Add testcase for exercise 2

--- unit-test.test.js
+++ unit-test.test.js
@@ -1,9 +1,16 @@
 const exerciseOne=require('./exercise-1')
+const exerciseTwo=require('./exercise-2')
 
+// For the first Exercise
 it("length meet the conditions",()=>{
     expect(exerciseOne("nahom")).toBe(5);
 })
 it("condition not met",()=>{
     expect(()=>exerciseOne("")).toThrow(Error)
     expect(()=>exerciseOne("nahomzerihun")).toThrow(Error)
+})
+
+// For the second Exercise
+it("Reverse a string",()=>{
+    expect(exerciseTwo("nahom")).toBe("mohan");
 })
